714fa00cc65e1e7917a30d34c08e89739c667ed7
Avihwr <41137189+Avihwr@users.noreply.github.com>
2021-07-08 23:11:38 +0530

    make it better

--- spotify/secrets.py
+++ spotify/secrets.py
@@ -1,3 +1,3 @@
 CLIENT_ID = "6b77244a582c47cd86408b0ca0a3c9a2"
 CLIENT_SECRET = '8ad6287636d24f4cb94f7c51d91fb1fd'
-REDIRECT_URI = "https://house-party-spotify.herokuapp.com"
+REDIRECT_URI = "https://house-party-spotify.herokuapp.com/spotify/redirect"
