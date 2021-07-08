03bb8474bd6cf694eb6a195a23a7dc4faeee623a
Avihwr <41137189+Avihwr@users.noreply.github.com>
2021-07-08 21:26:56 +0530

    make it better

--- house_party/settings.py
+++ house_party/settings.py
@@ -129,7 +129,7 @@ django_heroku.settings(locals())
 
 STATIC_URL = '/static/'
 
-
+# deployments
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
